615fc355fd7742586d80846e9c1cd29af2683503
marcilustra <theknifebox69@gmail.com>
2022-12-08 13:56:37 +0000

    Task 4, C1, 1 warning

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main (void) /*Main duh*/
+{
+	/*String initialization*/
+	char my_String[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(my_String);
+
+	return (0);
+}
