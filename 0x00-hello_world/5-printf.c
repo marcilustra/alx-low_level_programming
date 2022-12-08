8345a7d72566601d432fa35509889567320a487a
marcilustra <theknifebox69@gmail.com>
2022-12-08 14:28:08 +0000

    Task 5 Commit 1, 0EW

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*String Variable declarion*/
+	char message[] = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf(message);
+	return (0);
+}
