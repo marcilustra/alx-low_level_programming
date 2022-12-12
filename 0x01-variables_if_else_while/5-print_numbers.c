fbe41ed63e59b4dead31bdce8705272d08f5b11f
marcilustra <theknifebox69@gmail.com>
2022-12-12 12:30:44 +0000

    Mis numeros

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digit numbers
+ * with each num on a new line
+ * from 0 to 9
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	/*Variable init*/
+	int my_int;
+
+	my_int = 0;
+	while(my_int < 10) {
+		printf("%d", my_int);
+		my_int++;
+	}
+	printf("\n");
+
+	return (0);
+}
