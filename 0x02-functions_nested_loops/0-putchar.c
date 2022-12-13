7e2623c40e243308203d8ba589328507d5707030
marcilustra <theknifebox69@gmail.com>
2022-12-13 05:50:35 +0000

    Prints '_putchar'

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: Print out '_putchar'
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
