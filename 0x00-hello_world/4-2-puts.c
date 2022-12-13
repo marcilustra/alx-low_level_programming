a454cad5db26150200b73271c7083a8089aace73
marcilustra <theknifebox69@gmail.com>
2022-12-13 05:30:56 +0000

    Up to Date committance...is that a word?

---
+++ 0x00-hello_world/4-2-puts.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/*Main duh*/
+
+/**
+ *main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/*Int initialization*/
+	a = 0;
+	b = 10;
+
+	do {
+		printf("%d\n", a);
+		a++;
+	} while (a < b);
+	while (a = 0; a < b; a++)
+	{
+		 printf("%d\n", a);
+	}
+
+	return (0);
+}
