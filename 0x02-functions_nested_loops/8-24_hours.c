e753bfca96e167c911c1c92e827ba52545e8e5cf
marcilustra <theknifebox69@gmail.com>
2022-12-14 10:59:41 +0000

    Tick Tock

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer - Prints time from (00:00)
+ * to (23:59).
+ *
+ * Return: 0 (Void, Success)
+ */
+void jack_bauer(void)
+{
+	int hour, minutes;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+		}
+	}
+}
+
