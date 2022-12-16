08736c7b6fa14ed6b0a822b2ba26dec1b830676b
marcilustra <theknifebox69@gmail.com>
2022-12-16 22:47:26 +0000

    Multiplication of 2 integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - Multiplies two integers
+ * @a: first int passed
+ * @b: second int passed
+ * Return: 0 (Success) mulvalue
+ */
+int mul(int a, int b)
+{
+	int mulval;
+
+	mulval = (a * b);
+	return (mulval);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,4 +3,5 @@
 extern int _putchar(char c);
 extern int _isupper(int c);
 extern int _isdigit(int c);
+extern int mul(int a, int b);
 #endif
