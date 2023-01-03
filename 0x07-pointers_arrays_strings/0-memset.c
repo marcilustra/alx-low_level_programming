bb8ac6ff82c36c55180c6ace515cd2c024248252
marcilustra <theknifebox69@gmail.com>
2023-01-03 14:37:55 +0300

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - function to fill mem with constant.
+ * @s: pointer to character
+ * @b: char parameter that will change
+ * @n: position of array/ index
+ * Return: *s (succ)
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
