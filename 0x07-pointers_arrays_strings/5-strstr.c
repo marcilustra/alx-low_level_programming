8b039474543814889f457d526875444d57520afd
marcilustra <theknifebox69@gmail.com>
2023-01-03 16:08:46 +0300

    Number 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* _strstr - locates a substring
+* @haystack: larger string that is searched
+* through
+* @needle: substring being searched.
+* Return: 0 (Succ)
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *fneedle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+		haystack = result;
+	}
+	return (0);
+}
