d48319bdeb194a002806abe3b0179da7b2c91359
marcilustra <theknifebox69@gmail.com>
2023-01-05 15:44:56 +0300

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - print x raised to y
+ * @x: base
+ * @y: power
+ * Return: result (Succ)
+ */
+
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,8 +4,11 @@
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-int _strlen_recusrsion(char *s);
-
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt(int n, int i);
 
 #endif
 
