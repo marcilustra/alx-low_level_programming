7695f0fb5cd014252ca62717d2473d73c220ca06
marcilustra <theknifebox69@gmail.com>
2023-01-10 23:52:38 +0300

    We did this one in CS50X

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry, giving change to a customer
+ *
+ * @argc: count
+ * @argv: Vectors.
+ *
+ * Return: change in the form of coin count.
+ * -1 if error
+ *  0 if success.
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
+	}
+
+	printf("%d\n", coins);
+
+	return (0);
+}
