f994b14fb7c121b9e957e90cd7b808ca1c86ac68
marcilustra <theknifebox69@gmail.com>
2023-01-10 17:09:23 +0300

    Attempting the Stdio version

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Ikyk what this is.
+ * @argc: Parameter count.
+ * @argv: Parameter vectors.
+ * Return: 0 (succy)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s \n", argv[0]);
+
+	return (0);
+}
