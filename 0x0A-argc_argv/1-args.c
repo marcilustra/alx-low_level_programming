db0bce5edf3cb3e20cbb7a5719e0d9b683bb26d1
marcilustra <theknifebox69@gmail.com>
2023-01-10 17:29:01 +0300

    Bypass it

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,14 @@
-#include "main.h"
 #include <stdio.h>
+
+/**
+ * main - Entry
+ * @argc: Count
+ * @argv: Vectors.
+ * Return: 0 (Succ)
+ */
 
 int main(int argc, char *argv[])
 {
-	printf("%i\n", argc);
+	printf("%i\n", (argc - 1));
 	return (0);
 }
