7fc3606e064abf648fddd110b1ba517f70d0d664
marcilustra <theknifebox69@gmail.com>
2023-01-11 21:33:42 +0000

    empty commit

--- xPractice_Pointers/02-charithimetic.c
+++ xPractice_Pointers/02-charithimetic.c
@@ -11,4 +11,12 @@ int main(void)
 
 	printf("Character: %c.\nAddress: %p.\n", *pc, pc);
 	printf("Character: %c.\nNew address: %p.\n", *pc, pc + 1);
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		printf("Character: %c\nAddress: %p\n", *pc, pc);
+
+		//INcrementing the address has some unintended consequences.
+		//pc++;
+	}
 }
