bc4899e9daea325b3539a78953dd396be3341e08
marcilustra <theknifebox69@gmail.com>
2023-01-11 05:20:41 +0300

    A FizzBuzz type

--- 0x0A-argc_argv/pentsepty.c
+++ 0x0A-argc_argv/pentsepty.c
@@ -6,19 +6,32 @@ int main(int argc, char *argv[])
 {
 	//Counts
 	int i;
+	//Argument limit for the arithimetic.
 	int limit;
-	
+
+	//Checks for argument count existing
 	if (argc != 2)
 	{
-		printf("I need an integer to work\n");
+		printf("I need an int argument to work. Try Again!\n");
 		return (1);
 	}
+
+	//Convert the 2nd Argument to an int for working with.
 	limit = atoi(argv[1]);
+
+	//Check if int is negative and exit if it is.
+	if (limit <= 0)
+	{
+		printf("No Negatives or Non-digits please...Try Again!\n");
+		return (1);
+	}
+
+	//Perform PentySepty operation if int is +ve
 	for (i = 1; i < limit; i++)
 	{
-		if (i % 5 == 0 && i % 7 == 0)
+		if (i % 35 == 0)
 		{
-			printf("%d: Penty_Septy\n", i);
+			printf("%d: PentySepty\n", i);
 		}
 		else if( i % 7 == 0)
 		{
